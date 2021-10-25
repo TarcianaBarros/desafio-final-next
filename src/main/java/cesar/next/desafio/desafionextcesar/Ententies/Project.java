2209ff67c2d7166055e6c2645feecf7b61a2f090
Taciana <tarcianavbarros@gmail.com>
2021-10-25 20:32:48 -0300

    Atualizando entidades

--- src/main/java/cesar/next/desafio/desafionextcesar/Ententies/Project.java
+++ src/main/java/cesar/next/desafio/desafionextcesar/Ententies/Project.java
@@ -5,102 +5,34 @@ import cesar.next.desafio.desafionextcesar.Enum.FlagProject;
 import javax.persistence.*;
 import java.io.Serializable;
 
-@Table(name = "PROJETO")
 @Entity
+@Table(name = "PROJECT")
 public class Project implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID", nullable = false, unique = true)
     private Integer id;
 
-    @Column(name = "NOME_DO_PROJETO", nullable = false, unique = false)
+    @Column(name = "PROJECT_NAME", nullable = false, unique = false)
     private String projectName;
 
-    @Column(name = "DATA_DE_INICIO", nullable = false, unique = false)
+    @Column(name = "START_DATE", nullable = false, unique = false)
     private String startDate;
 
-    @Column(name = "DATA_FINAL", nullable = false, unique = false)
+    @Column(name = "FINAL_DATE", nullable = false, unique = false)
     private String finalDate;
 
     @Column(name = "STATUS", nullable = false, unique = false)
     private String status;
 
     @OneToMany
-    @JoinColumn(name = "usuario_id")
-    private Users users;
+    @JoinColumn(name = "USER_ID", referencedColumnName = "ID")
+    private User users;
+
 
     @Column(name = "FLAG", nullable = false, unique = false)
+    @Enumerated(EnumType.STRING)
     private FlagProject colors;
-
-    public Project(){
-
-    }
-    public Project(Integer id, String projectName, String startDate, String finalDate, String status, Users users, FlagProject colors){
-        this.id = id;
-        this.projectName = projectName;
-        this.startDate = startDate;
-        this.finalDate = finalDate;
-        this.users = users;
-        this.colors = colors;
-
-    }
-
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
-    }
-
-    public void setStartDate(String startDate) {
-        this.startDate = startDate;
-    }
-
-    public void setFinalDate(String finalDate) {
-        this.finalDate = finalDate;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public void setUsers(Users users) {
-        this.users = users;
-    }
-
-    public void setColors(FlagProject colors) {
-        this.colors = colors;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getProjectName() {
-        return projectName;
-    }
-
-    public String getStartDate() {
-        return startDate;
-    }
-
-    public String getFinalDate() {
-        return finalDate;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public Users getUsers() {
-        return users;
-    }
-
-    public FlagProject getColors() {
-        return FlagProject.toEnum(colors);
-    }
 
 
 
